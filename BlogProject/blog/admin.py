67b2576a382342e1a8a07ff90203517593935cfc
Hyacinthe <hyacinthemiamo5@gmail.com>
2022-12-08 09:33:52 +0100

    Update admin.py

--- BlogProject/blog/admin.py
+++ BlogProject/blog/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status','created_on')
-    list_filter = ("status",)
-    search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'slug', 'status','created_on')
+#     list_filter = ("status",)
+#     search_fields = ['title', 'content']
+#     prepopulated_fields = {'slug': ('title',)}
 
-admin.site.register(Post, PostAdmin)
+# admin.site.register(Post, PostAdmin)
+
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
