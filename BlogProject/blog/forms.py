87b2a13f3b6931f09420fa51e19304375c9997ea
Hyacinthe <hyacinthemiamo5@gmail.com>
2022-12-15 03:32:26 +0100

    Update forms.py

--- BlogProject/blog/forms.py
+++ BlogProject/blog/forms.py
@@ -3,6 +3,10 @@ from django import forms
 
 
 class CommentForm(forms.ModelForm):
+    """ 
+    name: CommentForm
+    description: This class give a form to be fill before commenting on a specific post
+    """
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
