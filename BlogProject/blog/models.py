683affa8b64c4d5bc0ecde24b9cb0c3ffe4e43d9
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-01-27 04:17:14 +0100

    Update models.py

--- BlogProject/blog/models.py
+++ BlogProject/blog/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now= True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    status = models.IntegerField(choices=STATUS, default=1)
 
     class Meta:
         ordering = ['-created_on']
