726ab1145e49aa2bfb0f37f59c01053b63da8103
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-02-10 00:27:19 +0100

    Update urls.py

--- BlogProject/blog/urls.py
+++ BlogProject/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     #path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('<slug:slug>/', views.post_detail, name='post_detail')
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
